03276cc5013c775372e8fe84e47c195810257a6b
lenmorelms <learnmorelms@gmail.com>
2023-12-19 03:07:20 +0200

    made mondodb url string

--- Server/config/MongoDb.js
+++ Server/config/MongoDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect("mongodb+srv://db_root:root@cluster0.xf3vqp8.mongodb.net/luluza_hair?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
